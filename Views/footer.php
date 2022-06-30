a1853d58c314864363fefddab1cb6f2ef516ec36
dimit <dimitriaigle@gmail.com>
2022-06-30 14:13:26 -0400

    modif affichage footer

--- Views/footer.php
+++ Views/footer.php
@@ -5,15 +5,15 @@
                 <img src="assert/logo.png" alt="miage-antilles" width="100" height="50">
             </a>
         </div>
-                <!--
-                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                    <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-                </a>
-                -->
-            <div class="col-md-4 justify-content-end list-unstyled d-flex">
-                <span class="text-muted"><?php date_default_timezone_set('UTC');echo date("Y")?> L3 MIAGE</span>
-            </div>
+        <!--
+        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        -->
+        <div class="col-md-4 justify-content-end list-unstyled d-flex">
+            <span class="text-muted"><?php date_default_timezone_set('UTC');echo date("Y")?> L3 MIAGE</span>
+        </div>
 
-</footer>
+    </footer>
 </div>
 </html>
