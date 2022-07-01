f635a3e10a97642d31d5bbf6e54f683ae1c597f3
GIONMJ <g17.nimajimbe@gmail.com>
2022-07-01 15:35:31 -0400

    php balise

---
+++ Views/questionnaire.php
@@ -0,0 +1,60 @@
+<?php
+include_once('header.php');
+?>
+
+<form>
+    <div>
+        <label>NOM:</label>
+        <label>Prénom:</label>
+    </div>
+    <div>
+        <textarea>
+            Quand vous disposez d’un moment de loisir, comment l’occupez vous ?
+        </textarea>
+        <textarea>
+            Dans quelle(s) autre(s) activité(s) aimeriez vous vous investir ?
+        </textarea>
+        <textarea>
+            Avez vous pratiqué des activités d’encadrement dans le contexte précédent ou d’autres ? Si oui, lesquelles ?
+        </textarea>
+        <textarea>
+            Qu’attendez vous de la MIAGE ?
+        </textarea>
+        <textarea>
+            Pourquoi la MIAGE des ANTILLES ?
+        </textarea>
+        <textarea>
+            L’exemple d’une personne de votre entourage a-t-il pesé sur votre choix ? Si oui dans quelle mesure ?
+        </textarea>
+        <textarea>
+            Citez au moins 3 métiers que vous pourriez exercer dès l’obtention de la Licence MIAGE
+(Classez-les par ordre de préférence)
+        </textarea>
+        <textarea>
+            A votre avis, quelles aptitudes personnelles faut-il développer pour exercer ces métiers ?
+        </textarea>
+        <textarea>
+            Y-aurait-il dans ce type de professions des aspects que vous appréhendez ? Si oui, pourquoi ?
+        </textarea>
+        <textarea>
+            Avez-vous eu l’occasion de découvrir des secteurs d’activités et des métiers cibles de la MIAGE par des visites ou des
+stages en entreprises ?
+        </textarea>
+        <textarea>
+            Dans quel(s) secteur(s) d’activités aimeriez-vous travailler ou ne pas travailler ?
+        </textarea>
+        <textarea>
+            Vous pouvez maintenant évoquer tout sujet ou toute question qui vous tient à cœur.
+        (Ne négligez pas ce paragraphe)
+        </textarea>
+        <input type="date">
+        <input type="file">
+
+    </div>
+</form>
+
+
+
+<?php
+include('footer.php');
+?>
