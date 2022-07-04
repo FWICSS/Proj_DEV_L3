e2b56f69cf181bb8e5638cf3d994b5238c6da07f
dimit <dimitriaigle@gmail.com>
2022-07-04 10:39:42 -0400

    debut fonction pdf

--- pdf/remplir_pdf.php
+++ pdf/remplir_pdf.php
@@ -6,9 +6,22 @@ $id = $_SESSION['idStudent'];
 $fields = array(
     'Current_année' => date('Y') ,
     'next_année' => date('Y')+1,
-    'nom' =>  $_SESSION['nom'] ,
-    'prenom' => $_SESSION['prenom'],
-    'date' => date('d/m/Y'),
+    'Nom' =>  $_SESSION['nom'] ,
+    'Prénom' => $_SESSION['prenom'],
+    'Nom_jeune_fille' => $_SESSION['nom'],
+    'Date_naissance' => $_SESSION['nom'],
+    'Lieu_naissance' => $_SESSION['nom'],
+    'Adresse_etudiant' => $_SESSION['nom'],
+    'Ville_E' => $_SESSION[''],
+    'Code_Postal_E' => $_SESSION['nom'],
+    'Email_E' => $_SESSION['nom'],
+    'Telephone_E' => $_SESSION['nom'],
+    'Mobile_E' => $_SESSION['nom'],
+    'Adresse_parents' => $_SESSION['nom'],
+    'Ville_P' => $_SESSION['nom'],
+    'Code_Postal_P' => $_SESSION['nom'],
+    'Email_P' => $_SESSION['nom'],
+    'Telephone_P' => $_SESSION['nom'],
 );
 
 //Le paramètre correspond au chemin vers le formulaire PDF
@@ -24,5 +37,6 @@ F pour sauvegarder le document en local
 S pour retourner le document en tant que chaine de caractère.
 Le deuxième paramètre est le nom du fichier
 */
-
-$pdf->Output("D",  $id+"questionnaire.pdf");
+$pdf->Output("I",  $id+"_questionnaire.pdf");
+$pdf->Output("F",  $id+"_questionnaire.pdf");
+$pdf->Output("D",  $id+"_questionnaire.pdf");
