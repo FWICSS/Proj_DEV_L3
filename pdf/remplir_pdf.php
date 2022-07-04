6fb20c0d41b921b6f2193838154675d1cd890748
dimit <dimitriaigle@gmail.com>
2022-07-04 10:13:25 -0400

    debut fonction pdf

--- pdf/remplir_pdf.php
+++ pdf/remplir_pdf.php
@@ -1,15 +1,18 @@
 <?php
 require('fpdm.php');
-
+global $database;
+$id = $_SESSION['idStudent'];
 //Tableau contenant les paramètres à passer au PDF
 $fields = array(
-    'nom' => 'Doe',
-    'prenom' => 'John',
+    'Current_année' => date('Y') ,
+    'next_année' => date('Y')+1,
+    'nom' =>  $_SESSION['nom'] ,
+    'prenom' => $_SESSION['prenom'],
     'date' => date('d/m/Y'),
 );
 
 //Le paramètre correspond au chemin vers le formulaire PDF
-$pdf = new FPDM('example_formulaire.pdf');
+$pdf = new FPDM('Candidature.pdf');
 $pdf->Load($fields, false); // le second paramètre vaut false si les valeurs sont en ISO-8859-1, vrai si UTF-8
 $pdf->Merge();
 
@@ -21,4 +24,5 @@ F pour sauvegarder le document en local
 S pour retourner le document en tant que chaine de caractère.
 Le deuxième paramètre est le nom du fichier
 */
-$pdf->Output("D", "example_formulaire.pdf");
+
+$pdf->Output("D",  $id+"questionnaire.pdf");
